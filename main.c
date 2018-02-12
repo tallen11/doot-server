ec1adfe0cd691eabbd365e070e4a519cbf186099
Tate Allen <tatea7@yahoo.com>
2018-02-12 17:58:27 +0000

    Actually looks ok now

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <stdint.h>
 // #include <sys/mman.h>
 #include <stdio.h>
+#include <time.h>
 // #include <rpi_ws281x/ws2811.h>
 #include "clk.h"
 #include "gpio.h"
@@ -15,8 +16,10 @@
 #include "version.h"
 #include "ws2811.h"
 
-const short PORT = 1153;
-const int REC_SIZE = 30;
+static short PORT = 1153;
+static int COLOR_COUNT = 3;
+static int LED_COUNT = 60;
+#define REC_SIZE (30 * COLOR_COUNT)
 
 int main() {
 	int s = socket(AF_INET, SOCK_DGRAM, 0);
@@ -48,7 +51,7 @@ int main() {
 	ws2811_channel_t c0;
 	c0.gpionum = 18;
 	c0.invert = 0;
-	c0.count = 60;
+	c0.count = LED_COUNT;
 	c0.strip_type = WS2811_STRIP_RGB;
 	c0.brightness = 255;
 
@@ -80,51 +83,61 @@ int main() {
 	for (int i = 0; i < REC_SIZE; ++i) {
 		targetBuf[i] = 0x0;
 	}
-	
+
+	clock_t lastTime = clock();
+
+	const float P_CONST = 0.25f;
+
 	for (;;) {
 		int recvlen = recvfrom(s, buf, REC_SIZE, MSG_DONTWAIT, (struct sockaddr*)&remaddr, &addrlen);
 		if (recvlen > 0) {
 			for (int i = 0; i < recvlen; ++i) {
 				targetBuf[i] = buf[i];
 			}
-		} // else {
-			for (int i = 0; i < REC_SIZE; ++i) {
-                        	unsigned char newVal = targetBuf[i] - currentBuf[i];
-                		float interp = 0.01f * (float)(newVal);
-                        	newVal = (unsigned char)interp;
-				// printf("%f ", interp);
+		}
 
-                        	uint32_t color = 0;
-                        	color = (color | newVal) << 4;
-                        	//color = (color | newVal) << 4;
-                        	//color = (color | newVal) << 4;
-                        	ledString.channel[0].leds[i] = color;
+		clock_t currentTime = clock();
+		float diff = ((float)(currentTime - lastTime) / (float)CLOCKS_PER_SEC) * 1000.0;
+		if (diff < (1.0f / 60.0f) * 1000.0f) {
+			continue;
+		}
 
-				currentBuf[i] = newVal;
-                	}
+		lastTime = currentTime;
 
-                	for (int i = 0; i < REC_SIZE; ++i) {
-                		unsigned char newVal = targetBuf[REC_SIZE - i] - currentBuf[REC_SIZE - i];
-                		float interp = 0.01f * (float)(newVal);
-                        	newVal = (unsigned char)interp;
+		for (int i = 0; i < REC_SIZE; i += COLOR_COUNT) {
+			float red_t = (float)targetBuf[i];
+			float green_t = (float)targetBuf[i+1];
+			float blue_t = (float)targetBuf[i+2];
 
-                        	uint32_t color = 0;
-                        	color = (color | newVal) << 4;
-                        	//color = (color | newVal) << 4;
-                        	//color = (color | newVal) << 4;
-                        	ledString.channel[0].leds[REC_SIZE + i] = color;
+			float red_c = (float)currentBuf[i];
+			float green_c = (float)currentBuf[i+1];
+			float blue_c = (float)currentBuf[i+2];
 
-				currentBuf[i] = newVal;
-                	}
+			float red_pid = (red_t - red_c) * P_CONST;
+			float green_pid = (green_t - green_c) * P_CONST;
+			float blue_pid = (blue_t - blue_c) * P_CONST;
 
-                	ret = ws2811_render(&ledString);
-                	if (ret != WS2811_SUCCESS) {
-                        // std::cerr << "Render error" << std::endl;
-                	}
-	//	}
+			uint8_t red = (uint8_t)(red_c + red_pid);
+			uint8_t green = (uint8_t)(green_c + green_pid);
+			uint8_t blue = (uint8_t)(blue_c + blue_pid);
 
-                //std::cout << std::endl;
+			uint32_t color = (0 << 24) | (red << 16) | (green << 8) | blue;
+			size_t index = i / 3;
+			
+			ledString.channel[0].leds[index] = color;
+			ledString.channel[0].leds[LED_COUNT - 1 - index] = color;
+
+			currentBuf[i] = red;
+			currentBuf[i+1] = green;
+			currentBuf[i+2] = blue;
+		}
+		
+                ret = ws2811_render(&ledString);
+                if (ret != WS2811_SUCCESS) {
+
+                }
 	}
 
 	return 0;
 }
+
