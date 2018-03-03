f918da2966c66258de10aaf37fd1f474334d3b87
Tate Allen <tatea7@yahoo.com>
2018-03-03 14:55:54 -0800

    Added comments to main.c

--- main.c
+++ main.c
@@ -50,19 +50,21 @@ int main() {
 	int lss = setupLEDStrip(&ledStrip);
 	assert(lss == 0);
 
+	// Init current color value buffer...
 	uint8_t currentBuf[REC_SIZE];
 	for (int i = 0; i < REC_SIZE; ++i) {
 		currentBuf[i] = 0;
 	}
 
+	// Init target color value buffer...
 	uint8_t targetBuf[REC_SIZE];
 	for (int i = 0; i < REC_SIZE; ++i) {
 		targetBuf[i] = 0;
 	}
 
 	clock_t lastTime = clock();
-
 	for (;;) {
+		// Get any new bytes from socket. Non-blocking so we can still render at refresh rate...
 		int recvlen = recvfrom(s, buf, REC_SIZE, MSG_DONTWAIT, (struct sockaddr*)&remaddr, &addrlen);
 		if (recvlen > 0) {
 			for (int i = 0; i < recvlen; ++i) {
@@ -70,6 +72,7 @@ int main() {
 			}
 		}
 
+		// Limit refresh rate of LED strip...
 		clock_t currentTime = clock();
 		float diff = ((float)(currentTime - lastTime) / (float)CLOCKS_PER_SEC) * 1000.0;
 		if (diff < (1.0f / REFRESH_RATE) * 1000.0f) {
@@ -78,34 +81,43 @@ int main() {
 
 		lastTime = currentTime;
 
+		// Update LED strip colors from received data...
 		for (int i = 0; i < REC_SIZE; i += COLOR_COUNT) {
+			// Target color values...
 			float red_t = (float)targetBuf[i];
 			float green_t = (float)targetBuf[i+1];
 			float blue_t = (float)targetBuf[i+2];
 
+			// Current color values...
 			float red_c = (float)currentBuf[i];
 			float green_c = (float)currentBuf[i+1];
 			float blue_c = (float)currentBuf[i+2];
 
+			// Simple P controller to smoothly move current color values to target values...
 			float red_pid = (red_t - red_c) * P_CONST;
 			float green_pid = (green_t - green_c) * P_CONST;
 			float blue_pid = (blue_t - blue_c) * P_CONST;
 
+			// Updated colors values...
 			uint8_t red = (uint8_t)(red_c + red_pid);
 			uint8_t green = (uint8_t)(green_c + green_pid);
 			uint8_t blue = (uint8_t)(blue_c + blue_pid);
 
+			// Create 32 bit color from individual channels...
 			uint32_t color = (0 << 24) | (red << 16) | (green << 8) | blue;
 			size_t index = i / 3;
 			
+			// Update LED strip...
 			ledStrip.channel[0].leds[index] = color;
 			ledStrip.channel[0].leds[LED_COUNT - 1 - index] = color;
 
+			// Save current colors for next time...
 			currentBuf[i] = red;
 			currentBuf[i+1] = green;
 			currentBuf[i+2] = blue;
 		}
 		
+		// Render the new colors...
         ws2811_return_t ret = ws2811_render(&ledStrip);
         if (ret != WS2811_SUCCESS) { }
 	}
